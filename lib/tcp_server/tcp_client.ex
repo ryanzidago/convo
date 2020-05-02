51f5d32fb54722ef1f3ad29e0fa7b877dad764ff
Ryan Zidago <ryanzidago@gmail.com>
2020-05-02 12:29:58 +0200

    refactored code; added possibility to change username

--- lib/tcp_server/tcp_client.ex
+++ lib/tcp_server/tcp_client.ex
@@ -14,15 +14,35 @@ defmodule TcpClient do
   def init(socket) do
     username = set_username(socket)
 
-    {:ok, %{username: username}}
+    state = %{username: username}
+
+    message = "#{username} has joined the chat!\n"
+    broadcast_info_message_to_others(socket, message)
+
+    {:ok, state}
   end
 
-  def handle_info({:tcp, socket, message}, %{username: username} = state) do
+  def handle_info(
+        {:tcp, socket, "> change-username: " <> new_username},
+        %{username: username} = state
+      ) do
+    new_username = String.trim(new_username)
+
+    broadcast_info_message_to_self(
+      socket,
+      "Your username has been changed from #{username} to #{new_username}\n"
+    )
+
+    message = "#{username} has changed his/her username to #{new_username}!"
+    broadcast_info_message_to_others(socket, message)
+
+    {:noreply, %{state | username: new_username}}
+  end
+
+  def handle_info({:tcp, socket, message}, state) do
     message = String.trim(message)
+    broadcast_to_others(socket, message, state)
 
-    Logger.info("Incoming packet: #{inspect(message)}")
-
-    broadcast_to_all_clients_except_author(socket, message, state)
     {:noreply, state}
   end
 
@@ -33,26 +53,31 @@ defmodule TcpClient do
   end
 
   defp set_username(socket) do
-    :gen_tcp.send(socket, "Please, provide your username: ")
+    broadcast_info_message_to_self(socket, "Please, provide a username: ")
+
     {:ok, username} = :gen_tcp.recv(socket, 0)
     :inet.setopts(socket, active: true)
 
     String.trim(username)
   end
 
-  defp display_prompt(socket, username) do
-    :gen_tcp.send(socket, "\n#{username} : ")
+  defp broadcast_info_message_to_self(author, message) do
+    :gen_tcp.send(author, message)
   end
 
-  defp broadcast_to_all_clients_except_author(author, message, %{username: username} = _state) do
+  defp broadcast_info_message_to_others(author, message) do
+    broadcast(author, message)
+  end
+
+  defp broadcast_to_others(author, message, %{username: username} = _state) do
     message = "#{username} : #{message}\n"
 
-    TcpClientPool.get_all_clients()
-    |> Stream.reject(&(&1 == author))
-    |> Enum.each(&broadcast(&1, message))
+    broadcast(author, message)
   end
 
-  defp broadcast(socket, line) do
-    :gen_tcp.send(socket, line)
+  defp broadcast(author, message) do
+    TcpClientPool.get_all_clients()
+    |> Stream.reject(&(&1 == author))
+    |> Enum.each(&:gen_tcp.send(&1, message))
   end
 end
